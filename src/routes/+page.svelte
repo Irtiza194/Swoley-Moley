bbc4a81db45962383c7cf7d9425e1aa927fceed4
Zayman <aimansuliman454@gmail.com>
2024-05-11 16:38:10 +0100

    gym website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Conversion from "../components/Conversion.svelte";
+  import Faq from "../components/FAQ.svelte";
+  import Footer from "../components/Footer.svelte";
+  import Reviews from "../components/Reviews.svelte";
+  import Hero from "./../components/Hero.svelte";
+  import Products from "./../components/Products.svelte";
+</script>
+
+<main class="flex flex-col">
+  <Hero></Hero>
+  <Products></Products>
+  <Reviews></Reviews>
+  <Faq></Faq>
+  <Conversion></Conversion>
+</main>
