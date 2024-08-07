07a114c84652bf2eee776d28e014b98d3b5606e3
Izhan <75379881+Irtiza194@users.noreply.github.com>
2024-08-07 13:30:41 +0100

    Final commit

--- src/hooks.js
+++
@@ -1,9 +0,0 @@
-export const handle = async ({ event, resolve }) => {
-    if (event.request.headers.get("Authorization") !== `Basic ${btoa("i194:bo55man66")}`) {
-        return new Response("401 Unauthorized", {
-            status: 401,
-            headers: { "WWW-Authenticate": 'Basic realm="User Visible Realm", charset="UTF-8"' },
-        });
-    }
-    else return resolve(event);
-};
